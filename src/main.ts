fc6bfdd787804320a0f8adeb0290e15b9862307b
tanvirAhmed73 <tanvir.073.ahmed@gmail.com>
2026-04-30 14:42:01 +0600

    updated package.json file for deploy

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ import { configureApp } from './app.bootstrap';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   configureApp(app);
-  await app.listen(process.env.PORT ?? 3000);
+  const port = Number(process.env.PORT) || 3000;
+  await app.listen(port, '0.0.0.0');
 }
 
 void bootstrap();
